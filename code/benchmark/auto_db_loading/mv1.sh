cb9906eb424653c473c243b32dc7ee134217b75a
Mengjia Kang <marjorie.kang@northwestern.edu>
2022-07-14 21:39:26 -0500

    add auto db loading bash script for model v1

---
+++ code/benchmark/auto_db_loading/mv1.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# clear the recording file
+cd ..
+cd ..
+
+> db_load_timev1.txt 
+echo 'mv1_load_time' >> db_load_timev1.txt
+
+cd python/venv
+
+for (( i=1; i<=30; i++))
+
+do
+
+    python auto_db_load.py
+
+    cd ..
+    cd ..
+    
+    #stop the db
+    ./bin/neo4j stop
+    sleep 15
+    
+
+    
+
+    printf -- "------------------- This is number $i run --------------------\n"
+
+    printf -- 'load started'
+
+    TIMEFORMAT=%R
+    ( time ./neo4j1.sh ) 2>> db_load_timev1.txt 
+
+    printf -- 'load finished'
+
+    # start the db
+    ./bin/neo4j start
+
+    #wait till the db fully start for the next load - macOS only take in seconds
+    sleep 60
+    # back to the python file directory in venv
+    cd python/venv/
+done
+
+
+ 
