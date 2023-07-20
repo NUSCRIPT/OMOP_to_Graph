efb2fe54a55cbdd5f5ccb4bfcbbb5c4167c0380c
Luke R <luke.rasmussen@gmail.com>
2023-07-20 09:57:41 -0500

    Add visit_occurrence_id to mv1 entity queries/export

--- code/omop-to-neo4j/mv1/has_procedure.sql
+++ code/omop-to-neo4j/mv1/has_procedure.sql
@@ -1,1 +1,1 @@
-\copy (select distinct person_id as ":START_ID(Person)", procedure_concept_id as ":END_ID(ProcedureOccurrence)", procedure_occurrence_id,procedure_date, 'HAS_PROCEDURE_OCCURRENCE' as ":TYPE" from public.procedure_occurrence) to 'has_procedure.csv' WITH CSV HEADER;
+\copy (select distinct person_id as ":START_ID(Person)", procedure_concept_id as ":END_ID(ProcedureOccurrence)", visit_occurrence_id, procedure_occurrence_id, procedure_date, 'HAS_PROCEDURE_OCCURRENCE' as ":TYPE" from public.procedure_occurrence) to 'has_procedure.csv' WITH CSV HEADER;
