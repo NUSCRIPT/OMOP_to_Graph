8c8dc511935d2e28dd052875d09079014736d6bc
Mengjia Kang <mengjiakang2017@u.northwestern.edu>
2023-10-03 17:46:33 -0500

    Update has_visit.sql

--- code/omop-to-neo4j/mv1/has_visit.sql
+++ code/omop-to-neo4j/mv1/has_visit.sql
@@ -1,1 +1,1 @@
-\copy (select person_id as ":START_ID(Person)", visit_concept_id as ":END_ID(VisitOccurrence)", visit_occurrence_id, visit_start_date, visit_end_date, 'HAS_VISIT_OCCURRENCE' as ":TYPE" from public.visit_occurrence v) to 'has_visit.csv' WITH CSV HEADER;
+\copy (select person_id as ":START_ID(Person)", visit_concept_id as ":END_ID(VisitOccurrence)", visit_occurrence_id, visit_start_date, visit_end_date, visit_source_value, 'HAS_VISIT_OCCURRENCE' as ":TYPE" from public.visit_occurrence v) to 'has_visit.csv' WITH CSV HEADER;
